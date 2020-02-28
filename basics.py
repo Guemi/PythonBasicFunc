a5cbc75fc4e9e12c1ee00f80be86704a17431675
Guemi <cracken_g95@hotmail.com>
2020-02-28 15:25:26 -0600

    Update basics.py

--- basics.py
+++ basics.py
@@ -3,3 +3,8 @@ n, m = map(int,input().split())
 
 #List comprenhension use
 obj = ["Even" if i%2==0 else "Odd" for i in range(10)]
+
+
+
+#List comprenhension example
+pattern = [('.|.'*(2*i + 1)).center(m, '-') for i in range(n//2)]
